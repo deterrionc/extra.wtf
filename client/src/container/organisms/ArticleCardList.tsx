82954b31a00fe2001c08649d475b73b0cad0b4f6
c-hillyard <hillyard.colleen@gmail.com>
2023-06-17 02:45:42 -0400

    fix: mobile responsive in homepage

--- client/src/container/organisms/ArticleCardList.tsx
+++ client/src/container/organisms/ArticleCardList.tsx
@@ -19,7 +19,7 @@ const ArticleCardList = ({ articles }: ArticleCardListProps) => {
       <Link to='/articles'>
         <h5 className="m-1 pb-3 text-lg font-bold leading-6">Articles <FaIcon iconName="fa-arrow-right" /></h5>
       </Link>
-      <div className="grid grid-cols-6 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+      <div className="grid grid-cols-2 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
         {_articles.length && _articles.map((article, index) =>
           <Article key={index} article={article} />
         )}
--- client/src/container/organisms/VideoCardList.tsx
+++ client/src/container/organisms/VideoCardList.tsx
@@ -19,7 +19,7 @@ const VideoCardList = ({ videos }: VideoCardListProps) => {
       <Link to='/videos'>
         <h5 className="m-1 pb-3 text-lg font-bold leading-6">Videos <FaIcon iconName="fa-arrow-right" /></h5>
       </Link>
-      <div className="grid grid-cols-6 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+      <div className="grid grid-cols-2 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
         {_videos.length && _videos.map((video, index) =>
           <Video key={index} video={video} />
         )}
