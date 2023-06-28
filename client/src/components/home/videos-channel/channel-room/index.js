1fb5597867e10c5d487aaa6f22456515f9c9df6e
c-hillyard <hillyard.colleen@gmail.com>
2023-06-28 16:29:25 -0400

    updated

--- client/src/components/home/videos-channel/channel-room/index.js
+++ client/src/components/home/videos-channel/channel-room/index.js
@@ -73,7 +73,7 @@ const ChannelRoom = ({ getChannel, channel }) => {
 
       <div className="absolute right-0 bottom-5 p-3 z-20 bg-white bg-opacity-50">
         <div className="flex justify-center">
-          <img src={`/${channel.image}`} alt={channel.name} className="w-24 sm:w-48 lg:w-64" />
+          <img src={`/${channel.image}`} alt={channel.name} className="w-24 sm:w-36 lg:w-48 aspect-[3/2]" />
         </div>
         <div className="text-center mb-0">{channel.name}</div>
       </div>
