6c52d295ad850cf01797d5d6490704dfc8022517
c-hillyard <hillyard.colleen@gmail.com>
2023-07-23 23:23:14 -0400

    image: fix

--- client/src/container/molecules/ChannelCard.tsx
+++ client/src/container/molecules/ChannelCard.tsx
@@ -1,25 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 interface ChannelProps {
   channel: {
-    _id: string,
-    name: string,
-    image: string,
-    videos: any,
-    slug: string
-  }
+    _id: string;
+    name: string;
+    image: string;
+    videos: any;
+    slug: string;
+  };
 }
 
 const ChannelCard = ({ channel }: ChannelProps) => {
   return (
-    // <Link to={"/video-channels/" + channel._id} className="m-1 width-calc bg-video-card rounded overflow-hidden">
-    <Link to={"/video-channels/" + channel.slug} className="m-1 width-calc bg-video-card rounded overflow-hidden">
-      <img alt="NOIMAGE" src={channel.image} className="w-full aspect-[3/2]" />
+    <Link
+      to={"/video-channels/" + channel.slug}
+      className="m-1 width-calc bg-video-card rounded overflow-hidden"
+    >
+      <div className="flex h-32 overflow-hidden">
+        <img
+          alt="NOIMAGE"
+          src={channel.image}
+          className="w-full"
+          style={{ height: "fit-content", minHeight: "-webkit-fill-available" }}
+        />
+      </div>
       <div className="p-2">
         <div className="text-base">{channel.name}</div>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default ChannelCard
+export default ChannelCard;
