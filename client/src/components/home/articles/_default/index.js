3c4be5f4d672737e544aafe4ec6e64c80d3be5a5
c-hillyard <hillyard.colleen@gmail.com>
2023-09-05 02:54:25 -0400

    fix: delete images in assets and fix lint warning

--- client/src/components/home/articles/_default/index.js
+++ client/src/components/home/articles/_default/index.js
@@ -1,18 +1,16 @@
-import {useEffect, Fragment} from 'react'
+import { useEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getArticles } from "../../../../actions/article";
-import ArticleCardList from '../../../../container/organisms/ArticleCardList';
+import ArticleCardList from "../../../../container/organisms/ArticleCardList";
 
 const Articles = ({ getArticles, articles }) => {
   useEffect(() => {
     getArticles();
   }, [getArticles]);
 
-  return (
-    <ArticleCardList articles={articles} />
-  )
-}
+  return <ArticleCardList articles={articles} />;
+};
 
 Articles.propTypes = {
   getArticles: PropTypes.func.isRequired,
@@ -22,6 +20,4 @@ const mapStateToProps = (state) => ({
   articles: state.article.articles,
 });
 
-export default connect(mapStateToProps, { getArticles })(
-  Articles
-);
+export default connect(mapStateToProps, { getArticles })(Articles);
