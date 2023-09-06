8008a04bf791a65d594858376b8a28e0f1d685f6
c-hillyard <hillyard.colleen@gmail.com>
2023-09-06 16:49:17 -0400

    fix: article and video card image size

--- client/src/container/molecules/VideoCard.tsx
+++ client/src/container/molecules/VideoCard.tsx
@@ -1,22 +1,33 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 interface VideoProps {
   video: {
-    id: string,
-    title: string,
-    image: string,
-  }
+    id: string;
+    title: string;
+    image: string;
+  };
 }
 
 const VideoCard = ({ video }: VideoProps) => {
   return (
-    <Link to={"/videos/" + video.id} className="m-1 width-calc bg-video-card rounded overflow-hidden">
-      <img alt={video.title} src={video.image} className="w-full aspect-[3/2]" />
-      <div className="p-2">
-        <div className="text-base">{video.title}</div>
+    <Link
+      to={"/videos/" + video.id}
+      className="m-1 width-calc bg-video-card rounded overflow-hidden"
+    >
+      <div className="m-1 width-calc bg-article-card rounded overflow-hidden cursor-pointer">
+        <div className="relative w-full aspect-[3/2] overflow-hidden">
+          <img
+            alt={video.title}
+            src={video.image}
+            className="absolute top-0 left-0 w-full h-full object-cover"
+          />
+        </div>
+        <div className="p-2">
+          <div className="text-base">{video.title}</div>
+        </div>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default VideoCard
+export default VideoCard;
