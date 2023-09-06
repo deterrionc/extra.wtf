b07655b07b02aca4f7ab360892220abcbcf7f737
c-hillyard <hillyard.colleen@gmail.com>
2023-09-06 16:46:29 -0400

    fix: image size

--- client/src/container/molecules/ArticleCard.tsx
+++ client/src/container/molecules/ArticleCard.tsx
@@ -15,11 +15,13 @@ const ArticleCard = ({ article }: ArticleProps) => {
   return (
     <Link to={article.link} target="_blank">
       <div className="m-1 width-calc bg-article-card rounded overflow-hidden cursor-pointer">
-        <img
-          alt={article.topic}
-          src={`/${article.path}/${article.image}`}
-          className="w-full aspect-[3/2]"
-        />
+        <div className="relative w-full aspect-[3/2] overflow-hidden">
+          <img
+            alt={article.topic}
+            src={`/${article.path}/${article.image}`}
+            className="absolute top-0 left-0 w-full h-full object-cover"
+          />
+        </div>
         <div className="p-2">
           <div className="text-base">{article.topic}</div>
           <h4 className="text-xl font-bold leading-6">{article.title}</h4>
